82b2f1bd24be2b455b312e2332670d3dd207f184
sitoplex <martimf@gmail.com>
2023-06-01 02:09:12 +0200

    Adding support for Zabbix port numbers and updates for Zabbix 6.4.2

--- agent_scripts/yum_update_agent_script.sh
+++ agent_scripts/yum_update_agent_script.sh
@@ -2,27 +2,80 @@
 # Author:       Moluss
 # Website       https://github.com/Moluss
 # Description:  Yum updates Monitor using Zabbix via command `yum check-update`
-#
+# 2023-05-31 updated to support Zabbix server ports
+
+# Server configuration file route
+AGENTD_CONF_FILE=/etc/zabbix_agentd.conf
+USE_ENCRYPTION=0
 
 # Route for tmp file to store yum output
 TEMP_ZBX_FILE=/tmp/zabbix_yum_check_output.tmp
 echo -n "" > $TEMP_ZBX_FILE
 
-ZBX_HOSTNAMEITEM_PRESENT=$(egrep ^HostnameItem /etc/zabbix_agentd.conf -c)
-ZBX_SERVERACTIVEITEM=$(egrep ^ServerActive /etc/zabbix_agentd.conf | cut -d = -f 2)
+if [ ! -f "$AGENTD_CONF_FILE" ]; then
+  echo "Error: File '$AGENTD_CONF_FILE' does not exist."
+  exit 1
+fi
+
+
+# Check if Server IP/name is set in configuration file
+ZBX_SERVERACTIVEITEM=$(egrep ^ServerActive $AGENTD_CONF_FILE | cut -d = -f 2)
 
 # Check if ServerActive is available
 if [ -z "$ZBX_SERVERACTIVEITEM" ]; then
    echo "Agent is not running on active mode"
-   exit -1
+   exit 1
+fi
+
+# Extract the hostname and port using pattern matching
+line=$(grep "^ServerActive=" $AGENTD_CONF_FILE)
+if echo "$line" | grep -Eq "ServerActive=([a-zA-Z0-9.-]+)(:([0-9]+))?$"; then
+
+  # Store the hostname
+  ZBX_SERVERACTIVEITEM=$(echo "$line" | sed -E 's/ServerActive=([a-zA-Z0-9.-]+)(:([0-9]+))?/\1/')
+
+  # Store the port
+  ZBX_SERVERACTIVEITEM_PORT=$(echo "$line" | sed -E 's/ServerActive=([a-zA-Z0-9.-]+)(:([0-9]+))?/\3/')
+  if [ -z "$ZBX_SERVERACTIVEITEM_PORT" ]; then
+    ZBX_SERVERACTIVEITEM_PORT="10051"
+  fi
+
+else
+  echo "Error: Unable to find the ServerActive line"
+  exit 1
 fi
 
 # Get hostname
+ZBX_HOSTNAMEITEM_PRESENT=$(egrep ^HostnameItem $AGENTD_CONF_FILE -c)
 if [ "$ZBX_HOSTNAMEITEM_PRESENT" -ge "1" ]; then
         ZBX_HOSTNAME=$(hostname)
 else
-        ZBX_HOSTNAME=$(egrep ^Hostname /etc/zabbix_agentd.conf | cut -d = -f 2)
+        ZBX_HOSTNAME=$(egrep ^Hostname $AGENTD_CONF_FILE | cut -d = -f 2)
 fi
+
+
+# Read the PSK identity and file from zabbix_agentd.conf
+if [ "$USE_ENCRYPTION" -ge "1" ]; then
+	psk_identity=$(sed -n 's/^TLSPSKIdentity[[:space:]]*=[[:space:]]*//p' $AGENTD_CONF_FILE | tr -d ' ')
+	psk_file=$(sed -n 's/^TLSPSKFile[[:space:]]*=[[:space:]]*//p' $AGENTD_CONF_FILE | tr -d ' ')
+	tls_connect=$(sed -n 's/^TLSConnect[[:space:]]*=[[:space:]]*//p' $AGENTD_CONF_FILE | tr -d ' ')
+	
+	if [[ -n $psk_identity ]]; then
+	        psk_identity="--tls-psk-identity $psk_identity"
+	fi
+	
+	if [[ -n $psk_file ]]; then
+	        psk_file="--tls-psk-file $psk_file"
+	fi
+	
+	if [[ -n $tls_connect ]]; then
+	        tls_connect="--tls-connect $tls_connect"
+	fi
+fi
+
+#######
+# YUM Update
+#######
 
 CHECK_UPDATES=$(yum check-update)
 # Check if updates are available TODO: If No packages marked for update matches or if an empty line is not found
@@ -44,11 +97,17 @@ done
 ########
 for i in "${array[@]}"
 do
-  echo "\"$ZBX_HOSTNAME\" yum.individualpackagetoupdate $i" >> $TEMP_ZBX_FILE
+  i=$(echo "$i" | tr -s ' ')
+  echo "\"$ZBX_HOSTNAME\" yum.individualpackagetoupdate \"$i\"" >> $TEMP_ZBX_FILE
 done
+
+# Remove line breaks using 'tr' command
+APT_UPDATES_SUMMARY=$(echo "$APT_UPDATES_SUMMARY" | tr -d '\n')
+# Replace multiple spaces with a single space
+APT_UPDATES_SUMMARY=$(echo "$APT_UPDATES_SUMMARY" | tr -s ' ')
 
 # Yum Update Full Summary
 echo "\"$ZBX_HOSTNAME\" yum.packagestoupdate.count $TOTAL_PACKAGES_COUNT" >> $TEMP_ZBX_FILE
-echo "\"$ZBX_HOSTNAME\" yum.packagestoupdate.description" $APT_UPDATES_SUMMARY >> $TEMP_ZBX_FILE
+echo "\"$ZBX_HOSTNAME\" yum.packagestoupdate.description \"$APT_UPDATES_SUMMARY\"" >> $TEMP_ZBX_FILE
+zabbix_sender -z $ZBX_SERVERACTIVEITEM -p $ZBX_SERVERACTIVEITEM_PORT $psk_identity $psk_file $tls_connect -i $TEMP_ZBX_FILE
 
-zabbix_sender -z $ZBX_SERVERACTIVEITEM -i $TEMP_ZBX_FILE
